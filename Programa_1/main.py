cbe5806bf2cfc43d85be2e113790e4f0d81265f1
Daniel Bustos Ruiz <daniel.bustos935@hotmail.com>
2024-01-25 22:19:41 -0600

    Terminado el programa y creado el archivo ejecutable

--- Programa_1/main.py
+++ Programa_1/main.py
@@ -1,5 +1,7 @@
 import os
 import time
+import sys
+from tabulate import tabulate
 
 # *Seccion de funciones de operaciones basicas
 def suma(a ,b):
@@ -50,16 +52,6 @@ while(True):
 numLotes = numProcesos // 4
 
 for i in range(0, numProcesos):
-  
-  # *Comprueba con multiplos de 4 para agregar a la lista de procesos cada lote
-  if i % 4 == 0 and i != 0:
-    procesos.append(auxLote)
-    auxLote = []
-  
-  # *En caso de que no se llegue ni a un lote o no se termine uno, guarda en automatico el ultimo lote que se estaba creando
-  if i == numProcesos - 1:
-    procesos.append(auxLote)
-    auxLote = []
 
   os.system("cls")
   nombre = input("Ingrese un nombre: ")
@@ -124,33 +116,96 @@ for i in range(0, numProcesos):
       os.system("cls")
       continue
 
+  if operacion == 1:
+    operacion = "Suma"
+  elif operacion == 2:
+    operacion = "Resta"
+  elif operacion == 3:
+    operacion = "Multiplicacion"
+  elif operacion == 4:
+    operacion = "Division"
+  elif operacion == 5:
+    operacion = "Residuo"
+
   # *Guarda todo en un diccionario y lo agrega a la lista de procesos
   auxLote.append({
     "nombre": nombre,
     "id": idUser,
     "operacion": operacion,
+    "fNum": fNum,
+    "sNum": sNum,
     "tiempo": tiempo,
     "resultado": 0
   })
 
-
-# !Bucle para imprimir los procesos
-contadorGlob = 0
-# for i in range(0, len(procesos)):
-#   for j in range(0, len(procesos[i])):
-#     print("------------------------------------")
-#     print(f"| Lote en ejecucion: {i + 1}        Contador global(seg.):{contadorGlob}", end="\r")
-#     print(f"------------------------------------")
-
-#     contadorGlob+=1
-#     # *Detiene el programa cada segundo
-#     time.sleep(1)
-
-for i in range(0, tiempoMax):
-  print("\033[K", end="")
-  print(f"------------------------------------")
-  print(f"| Lote en ejecucion: {i + 1}        Contador global(seg.):{contadorGlob}")
-  print(f"------------------------------------")
-  contadorGlob+=1
-  time.sleep(1)
+  # *Comprueba con multiplos de 4 para agregar a la lista de procesos cada lote
+  if (i+1) % 4 == 0:
+    procesos.append(auxLote)
+    auxLote = []
   
+  # *En caso de que no se llegue ni a un lote o no se termine uno, guarda en automatico el ultimo lote que se estaba creando
+  if i == numProcesos - 1:
+    procesos.append(auxLote)
+    auxLote = []
+
+columnas = ["Datos Generales:", "Lote en ejecucion:", "Proceso ejecutando:", "Procesos terminados:"]
+# !Bucle para imprimir los procesos
+contadorGlobal = 0
+for lote in procesos:
+  # *Lista que guarda los procesos terminados
+  procesosTerminadosList = []
+  for index, proceso in enumerate(lote):
+    # *Contador que lleva el numero de segundos del proceso
+    contadorProceso = 0
+    for i in range(0, proceso["tiempo"]):
+      
+      contadorGlobal += 1
+      #*Se declaran todas las lineas a mostrar en la tabla
+      noLotes = len(procesos) - 1
+      datosGenerales = f"No. lotes pendientes: {noLotes}\nContador global: {contadorGlobal}"
+      #*Se crea un string uniendose cada proceso del lote por un salto de linea, con el fin de imprimir todo el lote en una sola linea
+      loteEjecucion = "\n".join([f"ID: {proceso['id']} | Tiempo: {proceso['tiempo']}" for proceso in lote])
+
+      procesoEjecutando = f"ID: {proceso['id']}\nNombre: {proceso['nombre']}\nOperacion: {proceso['operacion']}\nTiempo estimado: {proceso['tiempo']}\nNo. proceso: {index+1}\nTiempo: {contadorProceso}\nTiempo restante: {proceso['tiempo'] - contadorProceso}"
+      contadorProceso += 1
+
+      # *Linea para mostrar todos los procesos terminados
+      if not procesosTerminadosList:
+        procesosTerminados = "No hay procesos\nterminados"
+      else:
+        procesosTerminados = "\n".join([f"Programa: {ind + 1}\nOperacion: {proceso['operacion']}\nDatos: {proceso['fNum']} {proceso['sNum']}\nResultado: {proceso['resultado']}\n" for ind, proceso in enumerate(procesosTerminadosList)])
+      
+      # !Se crea la tabla con los datos
+      fila = [datosGenerales, loteEjecucion, procesoEjecutando, procesosTerminados]
+      # *Se mueve el cursor al principio de la consola
+      sys.stdout.write('\033[H')
+      #* Se imprime la tabla
+      sys.stdout.write(tabulate([fila], headers=columnas, tablefmt='fancy_grid'))
+      #* Se limpia la consola
+      sys.stdout.flush()
+
+      #*Se agrega a la lista de procesos terminados
+      if proceso["tiempo"] - contadorProceso == 1:
+        # *Se realiza la operacion correspondiente
+        if proceso["operacion"] == "Suma":
+          proceso["resultado"] = suma(proceso["fNum"], proceso["sNum"])
+        elif proceso["operacion"] == "Resta":
+          proceso["resultado"] = resta(proceso["fNum"], proceso["sNum"])
+        elif proceso["operacion"] == "Multiplicacion":
+          proceso["resultado"] = multiplicacion(proceso["fNum"], proceso["sNum"])
+        elif proceso["operacion"] == "Division":
+          proceso["resultado"] = division(proceso["fNum"], proceso["sNum"])
+        elif proceso["operacion"] == "Residuo":
+          proceso["resultado"] = residuo(proceso["fNum"], proceso["sNum"])
+
+        procesosTerminadosList.append(proceso)
+
+      #* Se detiene el programa por un segundo
+      time.sleep(1)
+  
+  # *Justo antes de seguir con el siguiente lote, hace una pausa al programa con un input
+  input()
+  #* Se limpia la consola
+  sys.stdout.flush()
+  os.system("cls")
+    
