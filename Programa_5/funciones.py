49dd704b2553c5f9c09687d7bb70eebcdde565ce
DannyB935 <daniel.bustos935@hotmail.com>
2024-04-03 18:16:39 -0600

    El programa funciona como hasta la ultima actividad de FCFS continuacion

---
+++ Programa_5/funciones.py
@@ -0,0 +1,225 @@
+import random
+
+#!Funciones para generar datos aleatorios
+def getIds(p: int):
+  ids = set()
+  for i in range(0, p):
+    id = random.randint(1000, 9999)
+    #*Valida que sea unico y que no sea 0
+    if id not in ids and id > 0:
+      ids.add(id)
+  return ids
+  
+def getTiempos(p: int):
+  tiempos = []
+  for i in range(0, p):
+    tiempo = random.randint(5, 18)
+    if tiempo > 0:
+      tiempos.append(tiempo)
+  return tiempos
+
+def getOperaciones(p: int):
+  operaciones = []
+  for i in range(0, p):
+    a,b = random.randint(1, 1000), random.randint(1, 1000)
+    operacion = random.randint(1, 5)
+    if b == 0 and (operacion == 5 or operacion == 4):
+      #*Si la operacion invalida para los numeros, pone por defecto una suma
+      operacion = 1
+
+    if operacion == 1:
+      operacionStr = "+"
+    elif operacion == 2:
+      operacionStr = "-"
+    elif operacion == 3:
+      operacionStr = "*"
+    elif operacion == 4:
+      operacionStr = "/"
+    elif operacion == 5:
+      operacionStr = "%"
+
+    operaciones.append({
+      "fNum": a,
+      "sNum": b,
+      "operacion": operacion,
+      "operacionStr": operacionStr
+    })
+  return operaciones
+
+def getNewTiempo():
+  return random.randint(5, 18)
+
+def getNewId():
+  return random.randint(1000, 9999)
+
+def getNewOperacion():
+  operacion = random.randint(1, 5)
+  a, b = random.randint(1, 1000), random.randint(1, 1000)
+  if b == 0 and (operacion == 5 or operacion == 4):
+    operacion = 1
+
+  if operacion == 1:
+    operacionStr = "+"
+  elif operacion == 2:
+    operacionStr = "-"
+  elif operacion == 3:
+    operacionStr = "*"
+  elif operacion == 4:
+    operacionStr = "/"
+  elif operacion == 5:
+    operacionStr = "%"
+
+  return {
+    "fNum": a,
+    "sNum": b,
+    "operacion": operacion,
+    "operacionStr": operacionStr
+  }
+
+def generaProcesos(p: int):
+  auxProcesos = []
+  #*Genera copia de cada lista que obtiene
+  ids = getIds(p)
+  tiempos = getTiempos(p)[:]
+  operaciones = getOperaciones(p)[:]
+  for i in range(0, p):
+    tiempo = tiempos.pop()
+    operacion = operaciones.pop()
+    auxProcesos.append({
+      "id": ids.pop(),
+      "tiempo": tiempo,
+      "tiempoRestante": tiempo,
+      "operacion": operacion["operacion"],
+      "operacionStr": operacion["operacionStr"],
+      "fNum": operacion["fNum"],
+      "sNum": operacion["sNum"],
+      "resultado": 0,
+      "error": False,
+      "tiempoTrans": 0,
+      "tiempoLlegada": 0,
+      "tiempoFinalizacion": 0,
+      "tiempoRetorno": 0,
+      "tiempoEspera": 0,
+      "tiempoRespuesta": 0,
+      "tiempoServicio": 0,
+      "timeOut": 0,
+      "banderaRespuesta": False
+    })
+  return auxProcesos
+
+def generaProceso():
+  id = getNewId()
+  tiempo = getNewTiempo()
+  operacion = getNewOperacion()
+  return {
+    "id": id,
+    "tiempo": tiempo,
+    "tiempoRestante": tiempo,
+    "operacion": operacion["operacion"],
+    "operacionStr": operacion["operacionStr"],
+    "fNum": operacion["fNum"],
+    "sNum": operacion["sNum"],
+    "resultado": 0,
+    "error": False,
+    "tiempoTrans": 0,
+    "tiempoLlegada": 0,
+    "tiempoFinalizacion": 0,
+    "tiempoRetorno": 0,
+    "tiempoEspera": 0,
+    "tiempoRespuesta": 0,
+    "tiempoServicio": 0,
+    "timeOut": 0,
+    "banderaRespuesta": False
+  }
+
+#!Funciones para mostrar la tabla
+def getTablaEjecucion(procesosListos: list, procesosBloqueados: list, procesosNuevos: list, procesosTerminados: list, procesoEjecutar: dict, contadorGlobal: int):
+
+  #*Creo la linea a mostrar en la tabla
+  bloqueadosStr = ""
+  terminadosStr = ""
+  datosGenerales = f"Contador global: {contadorGlobal}\nProcesos nuevos: {len(procesosNuevos)}"
+  listosStr = "\n".join([f"ID: {proceso['id']} | Tiempo estimado: {proceso['tiempoRestante']} | Tiempo transcurrido: {proceso["tiempoTrans"]}" for proceso in procesosListos])
+  ejecutandoStr = f"ID: {procesoEjecutar['id']}\nOperacion: {procesoEjecutar["operacionStr"]}\nValores: {procesoEjecutar['fNum']} {procesoEjecutar['sNum']}\nTiempo transcurrido: {procesoEjecutar['tiempoTrans']}\nTiempo restante: {procesoEjecutar['tiempoRestante']}"
+
+  if len(procesosBloqueados) > 0:
+    bloqueadosStr = "\n".join([f"ID: {proceso['id']} | Tiempo restante: {7 - proceso['timeOut']}" for proceso in procesosBloqueados])
+
+  if len(procesosTerminados) > 0:
+    terminadosStr = "\n".join([f"ID: {proceso['id']} | Operacion: {proceso['operacionStr']}\nDatos: {proceso['fNum']} {proceso['sNum']} | {'Resultado: '+str(proceso['resultado']) if not proceso['error'] else 'Error'}\n" for proceso in procesosTerminados])
+
+  return [datosGenerales, listosStr, ejecutandoStr, bloqueadosStr, terminadosStr]
+
+def getTablaTerminado(procesosTerminados: list):
+  tablaFinal = []
+  for proceso in procesosTerminados:
+    lineaTiempos = f"Tiempo llegada: {proceso['tiempoLlegada']}\nTiempo finalizacion: {proceso['tiempoFinalizacion']}\nTiempo retorno: {proceso['tiempoRetorno']}\nTiempo espera: {proceso['tiempoEspera']}\nTiempo respuesta: {proceso['tiempoRespuesta']}\nTiempo servicio: {proceso['tiempoServicio']}"
+
+    tablaFinal.append([proceso['id'], proceso['operacionStr'], f"{proceso['fNum']} {proceso['sNum']}", f"{proceso['resultado'] if not proceso['error'] else 'Error'}", lineaTiempos])
+
+  return tablaFinal
+
+def getTablaPausa(procesosTerminados: list, procesosNuevos: list, procesosListos: list, procesosBloqueados: list, procesoEjecutar, contadorGlobal):
+
+  tablaPausa = []
+
+  if len(procesosTerminados) > 0:
+    for proceso in procesosTerminados:
+      tiemposTerminado = f"Tiempo llegada: {proceso['tiempoLlegada']}\nTiempo finalizacion: {proceso['tiempoFinalizacion']}\nTiempo retorno: {proceso['tiempoRetorno']}\nTiempo espera: {proceso['tiempoEspera']}\nTiempo respuesta: {proceso['tiempoRespuesta']}\nTiempo servicio: {proceso['tiempoServicio']}"
+      tablaPausa.append([proceso["id"], "Terminado", proceso["operacionStr"], f"{proceso['fNum']} {proceso['sNum']}", proceso["resultado"] if not proceso["error"] else "Error", tiemposTerminado])
+
+  if len(procesosNuevos) > 0:
+    for proceso in procesosNuevos:
+      tablaPausa.append([proceso["id"], "Nuevo", proceso["operacionStr"], f"{proceso['fNum']} {proceso['sNum']}", "No ha iniciado", "No ha iniciado"])
+
+  #*Procesos listos
+  if len(procesosListos) > 0:
+    for proceso in procesosListos:
+      calculaTiempos(proceso, contadorGlobal, 3)
+      tiemposListo = f"Tiempo llegada: {proceso['tiempoLlegada']}\nTiempo finalizacion: No terminado\nTiempo retorno: No terminado\nTiempo espera: {proceso['tiempoEspera']}\nTiempo respuesta: {proceso['tiempoRespuesta'] if not proceso['banderaRespuesta'] else "No ha iniciado"}\nTiempo servicio: {proceso['tiempoServicio']}\nTiempo restante CPU: {proceso['tiempoRestante']}"
+      tablaPausa.append([proceso["id"], "Listo", proceso["operacionStr"], f"{proceso['fNum']} {proceso['sNum']}", "No ha terminado", tiemposListo])
+
+  #*Procesos bloqueados
+  if len(procesosBloqueados) > 0:
+    for proceso in procesosBloqueados:
+      calculaTiempos(proceso, contadorGlobal, 2)
+      tiemposBloqueado = f"Tiempo llegada: {proceso['tiempoLlegada']}\nTiempo finalizacion: No terminado\nTiempo retorno: No terminado\nTiempo espera: {proceso['tiempoEspera']}\nTiempo respuesta: {proceso['tiempoRespuesta'] if not proceso['banderaRespuesta'] else 'No ha iniciado'}\nTiempo servicio: {proceso['tiempoServicio']}\nBloqueo restante: {7 - proceso['timeOut']}"
+      tablaPausa.append([proceso["id"], "Bloqueado", proceso["operacionStr"], f"{proceso['fNum']} {proceso['sNum']}", "No ha terminado", tiemposBloqueado])
+
+  if procesoEjecutar is not None:
+    calculaTiempos(procesoEjecutar, contadorGlobal, 4)
+    tiemposEjecutando = f"Tiempo llegada: {procesoEjecutar['tiempoLlegada']}\nTiempo finalizacion: No terminado\nTiempo retorno: No terminado\nTiempo espera: {procesoEjecutar['tiempoEspera']}\nTiempo respuesta: {procesoEjecutar['tiempoRespuesta']}\nTiempo servicio: {procesoEjecutar['tiempoServicio']}\nTiempo restante CPU: {procesoEjecutar['tiempoRestante']}"
+    tablaPausa.append([procesoEjecutar["id"], "Ejecutando", procesoEjecutar["operacionStr"], f"{procesoEjecutar['fNum']} {procesoEjecutar['sNum']}", "No ha terminado", tiemposEjecutando])
+
+  return tablaPausa
+
+#!Funciones para obtener los tiempos
+def calculaTiempos(proceso, contadorGlobal, tipo):
+  #?1 = terminado, 2 = bloqueado, 3 = listo, 4 = ejecutando
+  proceso["tiempoFinalizacion"] = contadorGlobal
+  proceso["tiempoRetorno"] = proceso["tiempoFinalizacion"] - proceso["tiempoLlegada"]
+  if proceso["error"] or tipo == 3 or tipo == 2 or tipo == 4:
+    proceso["tiempoServicio"] = proceso["tiempoTrans"]
+  else:
+    proceso["tiempoServicio"] = proceso["tiempo"]
+  if tipo == 3 or tipo == 2 or tipo == 4:
+    proceso["tiempoEspera"] = contadorGlobal - proceso["tiempoLlegada"] - proceso["tiempoServicio"]
+  else:
+    proceso["tiempoEspera"] = proceso["tiempoRetorno"] - proceso["tiempoServicio"]
+
+
+#!Funciones para las operaciones (resultados)
+def suma(a: int, b: int):
+  return a + b
+
+def resta(a: int, b: int):
+  return a - b
+
+def multiplicacion(a: int, b: int):
+  return a * b
+
+def division(a: int, b: int):
+  return a / b
+
+def modulo(a: int, b: int):
+  return a % b
---
+++ Programa_5/main.py
@@ -0,0 +1,158 @@
+from tabulate import tabulate
+import os
+import time
+import sys
+import msvcrt
+
+#*Modulos con las funciones
+from funciones import *
+
+while True:
+  try:
+    numProcesos = int(input("Ingrese el numero de procesos a realizar: "))
+    if numProcesos <= 0:
+      print("Por favor, ingrese un número entero positivo.")
+      input("Presione <enter> para continuar")
+      os.system("cls")
+      continue
+    break
+  except ValueError:
+    print("Por favor, ingrese un número entero.")
+    input("Presione <enter> para continuar")
+    os.system("cls")
+    continue
+
+#*Variables globales que se utilizan en toda la ejecucion
+columnasEjecucion = ["Datos Generales:", "Cola de listos:", "Proceso\nejecutando:", "Procesos\nbloqueados", "Procesos\nterminados:"]
+columnasTerminados = ["ID", "Operacion", "Datos", "Resultado", "Tiempos"]
+columnasPausa = ["ID", "Estado", "Operacion", "Datos", "Resultado", "Tiempos"]
+
+procesosNuevos = []
+procesosListos = []
+procesosBloqueados = []
+procesosTerminados = []
+contadorGlobal = 0
+global mostrarTabla
+global pausa
+mostrarTabla = False
+pausa = False
+
+procesosNuevos = generaProcesos(numProcesos)[:]
+
+#*Agrega los procesos nuevos a listos (maximo 4)
+while len(procesosListos) < 4 and len(procesosNuevos) > 0:
+  procesosListos.append(procesosNuevos.pop(0))
+  procesosListos[-1]["tiempoLlegada"] = contadorGlobal
+
+#!Bucle principal
+while len(procesosListos) > 0 or len(procesosBloqueados) > 0 or len(procesosNuevos) > 0:
+  
+  #*Toma el primer proceso de la lista de listos
+  if len(procesosListos) > 0:
+    procesoEjecutar = procesosListos.pop(0)
+
+  #*Comprueba si hay espacio libre en la memoria principal
+  if (len(procesosListos) + len(procesosBloqueados)) + 1 < 4 and len(procesosNuevos) > 0:
+    newListo = procesosNuevos.pop(0)
+    newListo["tiempoLlegada"] = contadorGlobal
+    procesosListos.append(newListo)
+
+  #!Ejecuta el proceso
+  while procesoEjecutar["tiempoRestante"] > 0:
+
+    if not procesoEjecutar["banderaRespuesta"]:
+      procesoEjecutar["tiempoRespuesta"] = contadorGlobal
+      procesoEjecutar["banderaRespuesta"] = True
+
+    contadorGlobal += 1
+    
+    #*Si todos estan en bloqueado, no tiene porque modificar estos valores
+    if len(procesosBloqueados) < 4:
+      procesoEjecutar["tiempoRestante"] -= 1
+      procesoEjecutar["tiempoTrans"] += 1
+
+    #*Actualiza los tiempos de los bloqueados
+    if len(procesosBloqueados) > 0:
+      for proceso in procesosBloqueados:
+        proceso["timeOut"] += 1
+        #*En caso de que se termine el tiempo de bloqueo
+        if proceso["timeOut"] == 8:
+          procesosListos.append(procesosBloqueados.pop(0))
+
+    #?Funciones en caso de presionar una tecla
+    if msvcrt.kbhit():
+      char = msvcrt.getch()
+      if char.lower() == b'p':
+        pausa = True
+      elif char.lower() == b'e':
+        if procesoEjecutar["tiempoRestante"] > 0:
+          procesosBloqueados.append(procesoEjecutar)
+        break
+      elif char.lower() == b'w':
+        procesoEjecutar["tiempoRestante"] = 0
+        procesoEjecutar["error"] = True
+        calculaTiempos(procesoEjecutar, contadorGlobal, 1)
+        procesosTerminados.append(procesoEjecutar)
+        procesoEjecutar = None
+        break
+      elif char.lower() == b'n':
+        #*Crea un nuevo proceso
+        #*Comprueba si hay espacio en la memoria principal
+        if (len(procesosListos) + len(procesosBloqueados)) + 1 < 4:
+          nuevoProceso = generaProceso()
+          nuevoProceso["tiempoLlegada"] = contadorGlobal
+          procesosListos.append(nuevoProceso)
+        else:
+          procesosNuevos.append(generaProceso())
+      elif char.lower() == b'b':
+        pausa = True
+        mostrarTabla = True
+    #*Bucle infinito hasta que se quite la pausa
+    if pausa:
+      while True:
+        if msvcrt.kbhit():
+          char = msvcrt.getch()
+          if char.lower() == b'c':
+            pausa = False
+            mostrarTabla = False
+            break
+        
+        if mostrarTabla:
+          #*Se imprime la tabla de pausa
+          sys.stdout.write('\033[H')
+          sys.stdout.write(tabulate(getTablaPausa(procesosTerminados, procesosNuevos, procesosListos, procesosBloqueados, procesoEjecutar, contadorGlobal)[:], headers=columnasPausa, tablefmt='fancy_grid'))
+          sys.stdout.flush()
+          time.sleep(0.1)
+
+    #*Muestra la tabla
+    linea = getTablaEjecucion(procesosListos, procesosBloqueados, procesosNuevos, procesosTerminados, procesoEjecutar, contadorGlobal)
+    sys.stdout.write('\033[H')
+    sys.stdout.write(tabulate([linea], headers=columnasEjecucion, tablefmt='fancy_grid'))
+    sys.stdout.flush()
+    time.sleep(1)
+
+  #*Proceso termina
+  if procesoEjecutar is not None and procesoEjecutar["tiempoRestante"] == 0:
+    if procesoEjecutar["error"]:
+      procesosTerminados.append(procesoEjecutar)
+    else:
+      if procesoEjecutar["operacion"] == 1:
+        procesoEjecutar["resultado"] = suma(procesoEjecutar["fNum"], procesoEjecutar["sNum"])
+      elif procesoEjecutar["operacion"] == 2:
+        procesoEjecutar["resultado"] = resta(procesoEjecutar["fNum"], procesoEjecutar["sNum"])
+      elif procesoEjecutar["operacion"] == 3:
+        procesoEjecutar["resultado"] = multiplicacion(procesoEjecutar["fNum"], procesoEjecutar["sNum"])
+      elif procesoEjecutar["operacion"] == 4:
+        procesoEjecutar["resultado"] = division(procesoEjecutar["fNum"], procesoEjecutar["sNum"])
+      elif procesoEjecutar["operacion"] == 5:
+        procesoEjecutar["resultado"] = modulo(procesoEjecutar["fNum"], procesoEjecutar["sNum"])
+      
+      #*Obtiene los tiempos
+      calculaTiempos(procesoEjecutar, contadorGlobal, 1)
+      procesosTerminados.append(procesoEjecutar)
+
+#!Imprime la tabla final
+os.system("cls")
+sys.stdout.write('\033[H')
+sys.stdout.write(tabulate(getTablaTerminado(procesosTerminados), headers=columnasTerminados, tablefmt='fancy_grid'))
+sys.stdout.flush()
